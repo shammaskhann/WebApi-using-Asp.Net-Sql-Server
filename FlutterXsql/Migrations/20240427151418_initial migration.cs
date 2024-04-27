04acd3b5208d16827a6b8478111221e7ce166de1
shammaskhann <shammas.csgo0312@gmail.com>
2024-04-27 12:17:29 -0700

    Add project files.

---
+++ FlutterXsql/Migrations/20240427151418_initial migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlutterXsql.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Class",
+                columns: table => new
+                {
+                    usedId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Class", x => x.usedId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Class");
+        }
+    }
+}
---
+++ FlutterXsql/Models/user.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlutterXsql.Models
+{
+    public class Users
+    {
+        [Key]
+        public int usedId { get; set; }
+        public String name { get; set; }
+
+        public String email { get; set; }   
+
+    }
+}
